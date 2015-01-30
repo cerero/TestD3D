b290a09fcd8c72b53f903e591c10e081dc52b4b3
cerero <cerero111@gmail.com>
2015-01-30 15:02:16 +0800

    3D游戏编程大师技巧

---
+++ stdafx.h
@@ -0,0 +1,47 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+#ifndef STDAFX_H
+#define STDAFX_H
+//#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             //  从 Windows 头文件中排除极少使用的信息
+// Windows 头文件: 
+#include <windows.h>
+
+// C 运行时头文件
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+// TODO:  在此处引用程序需要的其他头文件
+#include <windowsx.h> 
+#include <mmsystem.h>
+#include <objbase.h>
+#include <iostream> // include important C/C++ stuff
+#include <conio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <math.h>
+#include <io.h>
+#include <fcntl.h>
+#include <direct.h>
+#include <wchar.h>
+
+#include <sys/timeb.h>
+#include <time.h>
+
+#include <ddraw.h>  // directX includes
+#include <dinput.h>
+#include <dsound.h>
+#include <dmksctrl.h>
+//#include <dmusici.h>
+#include <dmusicc.h>
+//#include <dmusicf.h>
+
+#endif
